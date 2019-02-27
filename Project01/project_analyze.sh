3a3518f3c4fe08ebc94b18c58e5380710ce6d01d
ahmedt26 <ahmedt26@mcmaster.ca>
2019-02-27 04:20:41 +0000

    Completed script file, all features included. Removed images from project01 branch since it didn't seem to work last time.

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -2,7 +2,7 @@
 
 cd ..
 
-# Fancy script startup. It's nothing functional.
+# Fancy script startup. It's nothing functional. Trying to make it seem more like a UI.
 echo ' '
 echo '====================================================='
 echo '               ...Initializing GPPE...               '
@@ -27,7 +27,20 @@ echo '====================================================='
 sleep 0.5
 echo ' ' 
 
-selectScreen() { # Calls the available actions to output.
+# I made this UI to be fairly intuitive. The only reason for
+# someone to read the README is for detailed instructions
+# on what each of the features do, and not for UI-related help.
+# Regardless, I've iven instructions on how to use the UI
+# for time travellers.
+echo '====================================================='
+echo '   If this is your first time touching a computer... '
+sleep 0.5
+echo '        Nice time travel, read the README.           '
+echo '====================================================='
+
+sleep 0.5
+echo ' ' 
+selectScreen() { # Prints a menu which displays available actions.
 echo '====================================================='
 echo 'Select the action that best suits your needs:        '
 sleep 0.20
@@ -35,7 +48,9 @@ echo 'Type in code word to execute command                 '
 sleep 0.20
 echo 'Create TODO Log       - TODO                         '
 sleep 0.20
-echo 'File Type Count       - FTC (Not Implemented Yet)    '
+echo 'Last Modded File      - LMF                          '
+sleep 0.20
+echo 'File Type Count       - FTC                          '
 sleep 0.20
 echo 'Super Secret Function - IAMTHESENATE                 '
 sleep 0.20
@@ -43,8 +58,8 @@ echo 'End Script            - BYEBYE                       '
 echo '====================================================='
 }
 
-selectAction() { # Used to select which action to execute.
-	read code
+selectAction() { # Executes feature if correct code word is given, or asks to try again if given nothing or something else.
+	read -p 'Select a codeword: ' code
 	if [ $code = 'BYEBYE' ]; then
 		echo "You chose the codeword: $code"
 		byebye
@@ -56,9 +71,11 @@ selectAction() { # Used to select which action to execute.
 		toDo
 	elif [ $code = 'FTC' ]; then
 		echo "You chose the codeword: $code"
-		# fileTypeCount
+		fileTypeCount
 		echo ' I did not do this yet! :( Sorry. '
-		selectAction
+	elif [ $code = 'LMF' ]; then
+		echo "You chose the codeword: $code"
+		lastModdedFile
 	elif [ $code -eq 0 ]; then
 		echo "You gave me no input! Try again."
 		selectAction
@@ -68,61 +85,36 @@ selectAction() { # Used to select which action to execute.
 	fi
 }
 
-toDo() { # Finds all lines with #TODO in them and prints them in todo.log
+toDo() { # Creates a log of lines with #TODO in them along with their respective filepaths.
 	echo ' '
 	echo '====================================================='
 	echo 'Creating a To Do Log File and finding TODO items...'
-	rm -f ./Project01/todo.log
-	touch ./Project01/todo.log
-	grep -r '#TODO' --exclude="project_analyze.sh" "todo.log" . >> ./Project01/todo.log
+	rm -f ./Project01/logs/todo.log
+	touch ./Project01/logs/todo.log
+	grep -r '#TODO' --exclude="project_analyze.sh" --exclude="README.md" --exclude="todo.log" . >> ./Project01/logs/todo.log
 	sleep 0.25
 	echo 'Log file compliled!'
+	echo ' ' 
+	echo '=====================================================' # Reads file to user for convienence.
+	echo '                Here is what I found:                '
+	cat ./Project01/logs/todo.log
 	echo '====================================================='
 	echo ' '
 	selectScreen
 	selectAction
 }
 
-fileTypeCount() { # Finds require filetpes, not in working order yet
+fileTypeCount() { # Counts the number of filetype extensions in the repository. WIP
 	echo ' Counting filetypes in repository...'
-	filenames=$(find ./ -iname "*" -print0 | xargs -0 grep -ril "*" ) 
-	html=0
-	javascript=0
-	css=0
-	py=0
-	hs=0
-	bsh=0
-	for filename in $filenames ; do # Refactored code from data_collect.sh
-		echo ' '
-		echo '====================================================='
-		echo "Checking $filename"
-		if [ ${filename: -4} == ".css" ] ; then
-			echo "CSS count up"
-			css=$((css + 1))
-		fi
-		if [ ${filename: -3} == ".js" ] ; then
-			echo "JavaScript count up"
-			javascript=$((javascript + 1))
-		fi
-		if [ ${filename: -3} == ".py" ] ; then
-			echo "Python count up"
-			py=$((py + 1))
-		fi
-		if [ ${filename: -3} == ".hs" ] ; then
-			echo "Haskell count up"
-			hs=$((hs + 1))
-		fi
-		if [ ${filename: -3} == ".sh" ] ; then
-			echo "Bash count up"
-			bsh=$((bsh + 1))
-		fi
-		if [ ${filename: -5} == ".html" ] ; then
-			echo "HTML count up"
-			html=$((html + 1))
-		fi
-	done
 	
-	sleep 0.5
+	html=$(find . -name "*.html" | wc -l)
+	javascript=$(find . -name "*.js" | wc -l)
+	css=$(find . -name "*.css" | wc -l)
+	py=$(find . -name "*.py" | wc -l)
+	haskell=$(find . -name "*.hs" | wc -l)
+	bsh=$(find . -name "*.sh" | wc -l)
+	sleep 0.5 # The output: a fancy display of all the totals.
+	echo ' '
 	echo '====================================================='
 	echo '                       Results                       '
 	sleep 0.20
@@ -134,17 +126,67 @@ fileTypeCount() { # Finds require filetpes, not in working order yet
 	sleep 0.20
 	echo "Python: $py"
 	sleep 0.20
+	echo "Haskell: $haskell"
+	sleep 0.20
 	echo "Bash Script: $bsh"
 	sleep 0.20
+	echo ' ' 
 	echo 'Filetype Scan Complete!'
 	echo '====================================================='
 	echo ' '
-	selectScreen
+	selectScreen # Call selectScreen again to pick another ation.
 	selectAction
 }
 
-order66() {
+lastModdedFile() { # Finds all modified files within the last x minutes/hours/days in the current directory (project repo).
 	echo ' '
+	echo 'Tell me how far back you want to search for modified files, use integers please.'
+	sleep 1.0
+	echo 'Use M for minutes, H for hours and D for days.'
+	sleep 1.0
+	read -p 'M, H or D? ' unit # Read user's time frame.
+	read -p 'What interval? ' interval
+	if [ $unit = 'M' ] ; then
+		echo ' '
+		echo '====================================================='
+		echo "Searching for files modified within the last $interval minute(s)."
+		find . -mmin -$interval #-mmin takes the input as minutes, and the - before the number means less than or equal to.
+		echo ' '
+		echo 'All done!'
+		echo '====================================================='
+		echo ' ' 
+		selectScreen
+		selectAction
+	fi
+	if [ $unit = 'H' ] ; then
+		echo ' '
+		echo '====================================================='
+		echo "Searching for files modified within the last $interval hour(s)."
+		find . -mmin -$(($interval * 60))
+		echo ' '
+		echo 'All done!'
+		echo '====================================================='
+		echo ' '
+		selectScreen
+		selectAction
+	fi
+	if [ $unit = 'D' ] ; then
+		echo ' '
+		echo '====================================================='
+		echo "Searching for files modified within the last $interval day(s)."
+		find . -mtime -$interval # -mtime takes the input as number of days.
+		echo ' ' 
+		echo 'All done!'
+		echo '====================================================='
+		echo ' '
+		selectScreen
+		selectAction
+		
+	fi
+}
+order66() { # I am the senate. Prints the Tradedy of Darth Plaeuis Copypasta.
+	echo ' '
+	echo '====================================================='
 	echo 'Did you ever hear the tragedy of Darth Plagueis The Wise?'
 	sleep 1
 	echo 'I thought not.'
@@ -169,12 +211,14 @@ order66() {
 	sleep 1
 	echo 'He could save others from death, but not himself.'
 	echo ' '
+	echo '====================================================='
+	echo ' ' 
 	sleep 1
 	selectScreen
 	selectAction
 }
 
-byebye() {
+byebye() { # Exits script.
 	echo "Goodbye!"
 	sleep 0.25
 	exit 0;
@@ -182,6 +226,7 @@ byebye() {
 
 
 # Actual initialization of program functions. Will recursively go through actions until BYEBYE code used.
+
 selectScreen
 
 selectAction
